0eee6a5f57c0e1a132517f445edb08e17f65877e
Zaroxqs <zaroxqs@gmail.com>
2025-07-03 20:13:16 +0200

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Custom_games_launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.panelButtons = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.BackgroundButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.FolderButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Controls.Add(this.BackgroundButton);
+            this.panel1.Controls.Add(this.ClearButton);
+            this.panel1.Controls.Add(this.FolderButton);
+            this.panel1.Controls.Add(this.ExitButton);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(700, 54);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Select games folder";
+            // 
+            // panelButtons
+            // 
+            this.panelButtons.AutoScroll = true;
+            this.panelButtons.BackColor = System.Drawing.Color.Transparent;
+            this.panelButtons.Location = new System.Drawing.Point(13, 88);
+            this.panelButtons.Name = "panelButtons";
+            this.panelButtons.Size = new System.Drawing.Size(675, 500);
+            this.panelButtons.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Image|*.png, *jpg";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // BackgroundButton
+            // 
+            this.BackgroundButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BackgroundButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundButton.BackColor = System.Drawing.Color.Transparent;
+            this.BackgroundButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.BackgroundButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BackgroundButton.ForeColor = System.Drawing.Color.Transparent;
+            this.BackgroundButton.Image = global::ZGCL.Properties.Resources.BGImage;
+            this.BackgroundButton.Location = new System.Drawing.Point(138, 3);
+            this.BackgroundButton.Name = "BackgroundButton";
+            this.BackgroundButton.Size = new System.Drawing.Size(45, 45);
+            this.BackgroundButton.TabIndex = 4;
+            this.BackgroundButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.BackgroundButton.UseVisualStyleBackColor = false;
+            this.BackgroundButton.Click += new System.EventHandler(this.BackgroundButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ClearButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClearButton.BackColor = System.Drawing.Color.Transparent;
+            this.ClearButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClearButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ClearButton.ForeColor = System.Drawing.Color.Transparent;
+            this.ClearButton.Image = global::ZGCL.Properties.Resources.Clear;
+            this.ClearButton.Location = new System.Drawing.Point(66, 3);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(45, 45);
+            this.ClearButton.TabIndex = 3;
+            this.ClearButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ClearButton.UseVisualStyleBackColor = false;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // FolderButton
+            // 
+            this.FolderButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.FolderButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.FolderButton.BackColor = System.Drawing.Color.Transparent;
+            this.FolderButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.FolderButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.FolderButton.ForeColor = System.Drawing.Color.Transparent;
+            this.FolderButton.Image = global::ZGCL.Properties.Resources.Folder;
+            this.FolderButton.Location = new System.Drawing.Point(15, 3);
+            this.FolderButton.Name = "FolderButton";
+            this.FolderButton.Size = new System.Drawing.Size(45, 45);
+            this.FolderButton.TabIndex = 2;
+            this.FolderButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.FolderButton.UseVisualStyleBackColor = false;
+            this.FolderButton.Click += new System.EventHandler(this.FolderButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ExitButton.BackColor = System.Drawing.Color.Transparent;
+            this.ExitButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitButton.ForeColor = System.Drawing.Color.Transparent;
+            this.ExitButton.Image = global::ZGCL.Properties.Resources.Exit;
+            this.ExitButton.Location = new System.Drawing.Point(643, 1);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(50, 50);
+            this.ExitButton.TabIndex = 0;
+            this.ExitButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(700, 600);
+            this.ControlBox = false;
+            this.Controls.Add(this.panelButtons);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button FolderButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Panel panelButtons;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button BackgroundButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Custom_games_launcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // stuff for the pannel
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private List<string> gameNames = new List<string>();
+
+        // for persistent list
+        private static string SaveFilePath => Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "Zaros_CGL",
+            "gamelist.txt");
+
+        // for persistent bg image
+        private static string BgImageFilePath => Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "Zaros_CGL",
+            "bgimage.txt");
+
+
+        private void LoadGameList() // put the txt file for games in a list wanker
+        {
+            if (File.Exists(SaveFilePath))
+            {
+                gameNames = File.ReadAllLines(SaveFilePath).ToList();
+            }
+            else
+            {
+                gameNames = new List<string>();
+            }
+        }
+
+        private void SaveGameList() // you can read probably
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(SaveFilePath));
+            File.WriteAllLines(SaveFilePath, gameNames);
+        }
+
+        private void LoadBackground() // methoad to load the background quite obviously
+        {
+            if (File.Exists(BgImageFilePath))
+            {
+                string imgPath = File.ReadAllText(BgImageFilePath);
+                if (File.Exists(imgPath))
+                {
+                    try
+                    {
+                        this.BackgroundImage = new Bitmap(imgPath);
+                    }
+                    catch { }
+                }
+            }
+        }
+
+        // button class for the game icons
+        public class RoundedButton : Button
+        {
+            public int CornerRadius { get; set; } = 20;
+
+            protected override void OnPaint(PaintEventArgs pevent)
+            {
+                base.OnPaint(pevent);
+                var rect = this.ClientRectangle;
+                var path = GetRoundRectangle(rect, CornerRadius);
+                this.Region = new Region(path);
+
+                using (var pen = new Pen(this.BackColor, 2))
+                {
+                    pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                    pevent.Graphics.DrawPath(pen, path);
+                }
+            }
+
+            private GraphicsPath GetRoundRectangle(Rectangle rect, int radius)
+            {
+                var path = new GraphicsPath();
+                path.AddArc(rect.Left, rect.Top, radius, radius, 180, 90);
+                path.AddArc(rect.Right - radius, rect.Top, radius, radius, 270, 90);
+                path.AddArc(rect.Right - radius, rect.Bottom - radius, radius, radius, 0, 90);
+                path.AddArc(rect.Left, rect.Bottom - radius, radius, radius, 90, 90);
+                path.CloseFigure();
+                return path;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // round corners
+            int radius = 30;
+            var path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(new Rectangle(0, 0, radius, radius), 180, 90);
+            path.AddArc(new Rectangle(this.Width - radius, 0, radius, radius), 270, 90);
+            path.AddArc(new Rectangle(this.Width - radius, this.Height - radius, radius, radius), 0, 90);
+            path.AddArc(new Rectangle(0, this.Height - radius, radius, radius), 90, 90);
+            path.CloseFigure();
+            this.Region = new Region(path);
+
+            // button shenanigans
+            ExitButton.FlatAppearance.BorderSize = 0;
+            ExitButton.FlatAppearance.MouseOverBackColor = ExitButton.BackColor;
+            ExitButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(90, 0, 120);
+            FolderButton.FlatAppearance.BorderSize = 0;
+            FolderButton.FlatAppearance.MouseOverBackColor = FolderButton.BackColor;
+            FolderButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(90, 0, 120);
+            ClearButton.FlatAppearance.MouseOverBackColor = ClearButton.BackColor;
+            ClearButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(90, 0, 120);
+            ClearButton.FlatAppearance.BorderSize = 0;
+            BackgroundButton.FlatAppearance.MouseOverBackColor = ClearButton.BackColor;
+            BackgroundButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(90, 0, 120);
+            BackgroundButton.FlatAppearance.BorderSize = 0;
+
+            // load stuff
+            LoadGameList();
+            GenerateButtons();
+            LoadBackground();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // draggable panel
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        private void FolderButton_Click(object sender, EventArgs e)
+        {
+            using (var d = folderBrowserDialog1)
+            {
+                if (d.ShowDialog() == DialogResult.OK)
+                {
+                    using (var dialog = new FolderBrowserDialog())
+                    {
+                        if (dialog.ShowDialog() == DialogResult.OK)
+                        {
+                            gameNames.Clear();
+                            string[] subfolders = Directory.GetDirectories(dialog.SelectedPath);
+                            gameNames.AddRange(subfolders);
+                            SaveGameList();
+                            GenerateButtons();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn?.Tag is ValueTuple<string, string> tag)
+            {
+                string exePath = tag.Item1;
+                string folderPath = tag.Item2;
+
+                try
+                {
+                    if (!string.IsNullOrEmpty(exePath) && File.Exists(exePath))
+                    {
+                        System.Diagnostics.Process.Start(exePath);
+                    }
+                    else
+                    {
+                        System.Diagnostics.Process.Start("explorer.exe", folderPath);
+                    }
+                }
+                catch
+                {
+                    try { System.Diagnostics.Process.Start("explorer.exe", folderPath); }
+                    catch { MessageBox.Show("Could not open folder or launch executable."); }
+                }
+            }
+        }
+
+        private void GenerateButtons()
+        {
+            panelButtons.Controls.Clear();
+            int btnSize = 100;
+            int margin = 20;
+            int x = margin;
+            int y = margin;
+            int buttonsPerRow = (panelButtons.Width - margin) / (btnSize + margin);
+
+            for (int i = 0; i < gameNames.Count; i++)
+            {
+                string folderPath = gameNames[i];
+                string folderName = Path.GetFileName(folderPath);
+
+                string exePath = null;
+                Bitmap iconBitmap = null;
+
+                if (Directory.Exists(folderPath))
+                {
+                    string[] exes = Directory.GetFiles(folderPath, "*.exe");
+                    if (exes.Length > 0)
+                    {
+                        exePath = exes[0];
+                        try
+                        {
+                            using (Icon icon = Icon.ExtractAssociatedIcon(exePath))
+                            {
+                                if (icon != null)
+                                    iconBitmap = icon.ToBitmap();
+                            }
+                        }
+                        catch { /* ignore */ }
+                    }
+                }
+
+                var btn = new RoundedButton
+                {
+                    Width = btnSize,
+                    Height = btnSize,
+                    CornerRadius = 15,
+                    Text = "",
+                    Location = new Point(x, y),
+                    BackColor = Color.LightBlue,
+                    FlatStyle = FlatStyle.Flat,
+                    Image = iconBitmap,
+                    ImageAlign = ContentAlignment.MiddleCenter,
+                    TextAlign = ContentAlignment.BottomCenter,
+                    Tag = (exePath, folderPath) 
+                };
+
+                new ToolTip().SetToolTip(btn, folderName);
+
+                btn.Click += Btn_Click;
+
+                panelButtons.Controls.Add(btn);
+
+                x += btnSize + margin;
+                if ((i + 1) % buttonsPerRow == 0)
+                {
+                    x = margin;
+                    y += btnSize + margin;
+                }
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(SaveFilePath))
+                {
+                    File.Delete(SaveFilePath);
+                }
+            }
+            catch { }
+
+            try
+            {
+                if (File.Exists(BgImageFilePath))
+                {
+                    File.Delete(BgImageFilePath);
+                }
+            }
+            catch { }
+
+            gameNames.Clear();
+
+            panelButtons.Controls.Clear();
+            this.BackgroundImage = null;
+        }
+
+        private void BackgroundButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Select Image";
+            openFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (this.BackgroundImage != null)
+                        this.BackgroundImage.Dispose();
+
+                    this.BackgroundImage = new Bitmap(openFileDialog1.FileName);
+
+                    Directory.CreateDirectory(Path.GetDirectoryName(BgImageFilePath));
+                    File.WriteAllText(BgImageFilePath, openFileDialog1.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Exception setting image:\n" + ex.Message);
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
